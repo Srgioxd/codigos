4ff65e5f5fa24d7b1e3ff3a809f720166647c4e2
S E R G I O <Srgioxd>
2022-11-14 18:09:40 -0500

    xdnt

---
+++ Question.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question : MonoBehaviour
+{
+    public string       text    = null;
+    public List<Option> options = null;
+        
+
+}
